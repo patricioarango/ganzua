896327894f518c4c69b22fed1a4f8ecb587f55fd
patricioarango <patricioarango@gmail.com>
2016-06-29 15:51:45 -0300

    two

--- www/js/index.js
+++ www/js/index.js
@@ -53,6 +53,18 @@ var app = {
     onDeviceReady: function() {
         angular.element(document).ready(function() {
             angular.bootstrap(document);
+
+   cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );            
         });
     },
 };
