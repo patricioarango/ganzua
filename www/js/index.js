90fc2cf85b9074a6217bcf5bb63696ad830a4e73
patricioarango <patricioarango@gmail.com>
2016-06-30 20:50:56 -0300

    two

--- www/js/index.js
+++ www/js/index.js
@@ -78,7 +78,7 @@ function loguearUsuario(){
     console.log("aca");
 window.plugins.googleplus.login(
     {
-      'scopes': '', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+      'scopes': 'https://www.googleapis.com/auth/plus.login', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
       'webClientId': '1071912290900-3qmnrom1qg0hgjk3j6cd9evv893gauma.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
       'offline': false, // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
     },
