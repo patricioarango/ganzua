720804e1407afc76dafc91501b5b38b06db58aff
patricioarango <patricioarango@gmail.com>
2016-06-30 14:50:13 -0300

    two

--- www/js/index.js
+++ www/js/index.js
@@ -72,17 +72,24 @@ function getQrCode(){
 function loguearUsuario(){
   var provider = new firebase.auth.GoogleAuthProvider();
   provider.addScope('https://www.googleapis.com/auth/plus.login');
-  firebase.auth().signInWithRedirect(provider);
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
 
 }
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    var token = result.credential.accessToken;
-  }
-  var user = result.user;
-}).catch(function(error) {
-  console.log(error);
-});
 
 function desloguearUsuario(){
   firebase.auth().signOut().then(function(result) {
