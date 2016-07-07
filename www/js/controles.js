951cbc1f7b4e76c1f81156e3a05afbbbca5c0c6c
patricioarango <patricioarango@gmail.com>
2016-07-07 07:45:13 -0300

    two

--- www/js/controles.js
+++ www/js/controles.js
@@ -1,5 +1,15 @@
 aplicacion.controller('mainController',['$scope','$location','$http', function($scope, $location,$http){
-
+	$scope.loguearUsuarioGoogle = function(){
+	window.plugins.googleplus.login(
+          {},
+          function (obj) { 
+            alert(obj);
+          },
+          function (msg) {
+            loguearUsuarioGoogle();
+          }
+  	);	
+	}
 }]);
 
 aplicacion.controller('profileController',['$scope', '$routeParams', '$http','$sce','$rootScope',
