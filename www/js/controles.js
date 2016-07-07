b8ebcfe428c4b9eec0fcbf1c958b7adfe712f4f6
patricioarango <patricioarango@gmail.com>
2016-07-07 09:33:22 -0300

    two

--- www/js/controles.js
+++ www/js/controles.js
@@ -1,15 +1,5 @@
 aplicacion.controller('mainController',['$scope','$location','$http', function($scope, $location,$http){
-	$scope.loguearUsuarioGoogle = function(){
-	window.plugins.googleplus.login(
-          {},
-          function (obj) { 
-            alert(obj);
-          },
-          function (msg) {
-            loguearUsuarioGoogle();
-          }
-  	);	
-	}
+console.log("en controllador");
 }]);
 
 aplicacion.controller('profileController',['$scope', '$routeParams', '$http','$sce','$rootScope',
