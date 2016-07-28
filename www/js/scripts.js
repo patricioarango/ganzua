a7fbcf9f85fb300f805acd37857478e0ac8850ba
patricioarango <patricioarango@gmail.com>
2016-07-28 13:26:13 -0300

    two

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -33,15 +33,79 @@ $("#enviar_email").on('click', function(event) {
   window.localStorage.setItem("ganzua_email",email);
   var email_name   = email.substring(0, email.lastIndexOf("@"));
   var deviceid = window.localStorage.getItem("ganzua_deviceid");
-  window.location.href = "http://autowikipedia.es/google_login/index/" + email_name + "/" + deviceid;
+  if (usuario_habilitado(email)){
+    loguear_usuario_firebase();
+    var uid = window.localStorage.getItem("ganzua_uid");    
+    window.location.href = "http://autowikipedia.es/google_login/index/" + uid;
+  } else {
+
+  }
 });
 
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBvt5tGBZb3uaqZnjLmAOWFPtJcGd1nSGo",
+  authDomain: "ganzua-eea1d.firebaseapp.com",
+  databaseURL: "https://ganzua-eea1d.firebaseio.com",
+  storageBucket: "ganzua-eea1d.appspot.com",
+};
+  var appfire = firebase.initializeApp(config);
+  var db = appfire.database();
 
+appfire.auth().signInAnonymously().catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+});
 
+firebase.auth().onAuthStateChanged(function(user) {
+  console.log("logueando usuario anonimo");
+  if (user) {
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    window.localStorage.setItem("ganzua_uid",uid);
+  } else {
+    // User is signed out.
+    // ...
+  }
+  // ...
+});
 
+function usuario_habilitado(email){
+  var res = false;
+  //traemos la whiteliste de los usuarios habilitados para ganzua
+  var whitelist = [];
+  //traemos el whitelist
+  db.ref('/whitelist').once('value').then(function(snapshot) {
+    console.log("chequeando si es usuario habilitado...");
+    var username = snapshot.val();
+    $.each(username, function(index, val) {
+      console.log(val + "es igual a..." + email);
+       if (val == email) {
+        console.log("alcoyana - alcoyana");
+        console.log("estás habilitado, tomá tu token");
+        res = true;
+       }
+    });
+    if (res === false){
+      console.log("habla con el capo para que te habilite");
+      ("#no_authorized_card").show();
+    }     
+  });
+}
 
-
-
+function loguear_usuario_firebase(){
+  console.log("guardando usuario firebase");
+  var uid = window.localStorage.getItem("ganzua_uid");
+  var deviceid = window.localStorage.getItem("ganzua_deviceid");
+  var email = window.localStorage.getItem("ganzua_email");
+  db.ref("appusers").set({
+    uid: userId,
+    deviceid: deviceid,
+    email: email
+  });
+}
 
 
 
